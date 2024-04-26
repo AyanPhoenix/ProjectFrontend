17500725000d633336a4d22ec47fdd1b62d8e16f
Asif <asifhaque0505@gmail.com>
2024-04-26 22:00:07 +0530

    .

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
-var cors = require("cors"); 
-const jwt=require('jsonwebtoken');
-const multer=require('multer');
-const path = require('path'); 
-const fs = require('fs');
-const cookieparser=require('cookie-parser');
-require('dotenv').config();
+var cors = require("cors");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const cookieparser = require("cookie-parser");
+require("dotenv").config();
 
-const uploadDir = path.join(__dirname, 'uploads');
+const uploadDir = path.join(__dirname, "uploads");
 
 if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
+  fs.mkdirSync(uploadDir);
 }
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); 
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname); 
-  }
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
 const upload = multer({ storage: storage });
 
 const app = express();
 const port = 3000;
-          
 
 mongoose
   .connect("mongodb://localhost:27017/UserDB")
@@ -38,22 +37,26 @@ mongoose
     console.error("Error connecting to MongoDB:", err);
   });
 
-
-  const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: String,
     email: String,
     password: String,
-    tokens:[{
-      token:{
-        type:String,
-        required:true,
-      }
-    }]
-  }, { collection: 'users' });
-  
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  { collection: "users" }
+);
+
 const User = mongoose.model("User", userSchema);
 
-const generateAuthToken = async(userId) => {
+const generateAuthToken = (userId) => {
   try {
     const token = jwt.sign({ _id: userId }, process.env.SECRET_KEY);
     return token;
@@ -62,33 +65,37 @@ const generateAuthToken = async(userId) => {
   }
 };
 
-const profileSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+const profileSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    profilePicture: String,
+    productImages: [
+      {
+        image: String,
+        caption: String,
+      },
+    ],
+    skills: String,
+    expertise: String,
   },
-  profilePicture: String,
-  productImages: [{
-    image: String,
-    caption: String
-  }],
-  skills: String,
-  expertise: String
-}, { collection: 'profiles' });
+  { collection: "profiles" }
+);
 
-const Profile = mongoose.model('Profile', profileSchema);
-
+const Profile = mongoose.model("Profile", profileSchema);
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieparser());
 
-app.post("/register", async(req, res) => {
+app.post("/register", async (req, res) => {
   const userData = req.body;
   const existingUser = await User.findOne({ username: userData.username });
   const existingemail = await User.findOne({ email: userData.email });
-  
+
   if (existingUser) {
     return res.status(401).json({ message: "User already exists" });
   } else if (existingemail) {
@@ -98,12 +105,18 @@ app.post("/register", async(req, res) => {
       const user = await User.create(userData);
       const token = generateAuthToken(user._id);
       console.log("Token:", token);
-      res.cookie("jwt",token ,{
-        expires:new Date(Date.now()+30000),
-        httpOnly:true,
+      res.cookie("jwt", token, {
+        expires: new Date(Date.now() + 30000),
+        httpOnly: true,
       });
 
-      return res.status(201).json({ token: generateAuthToken(user._id), userId: user._id, message: `${userData.username} added` });
+      return res
+        .status(201)
+        .json({
+          token: generateAuthToken(user._id),
+          userId: user._id,
+          message: `${userData.username} added`,
+        });
     } catch (err) {
       console.error(err);
       return res.status(500).json({ message: "Internal Server Error" });
@@ -111,19 +124,19 @@ app.post("/register", async(req, res) => {
   }
 });
 
-app.post('/upload', upload.single('file'), (req, res) => {
+app.post("/upload", upload.single("file"), (req, res) => {
   try {
     const file = req.file;
     if (!file) {
-      return res.status(400).json({ message: 'No file uploaded' });
+      return res.status(400).json({ message: "No file uploaded" });
     }
 
     const fileUrl = `http://localhost:3000/uploads/${file.filename}`;
 
     res.status(200).json({ imageUrl: fileUrl });
   } catch (error) {
-    console.error('Error uploading file:', error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    console.error("Error uploading file:", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
@@ -145,20 +158,23 @@ app.post("/login", async (req, res) => {
       return res.status(401).json({ message: "Token is required" });
     }
 
-    const decodedToken = jwt.verify(token, 'thisisthesecuritytokengeneratedbyme');
+    const decodedToken = jwt.verify(
+      token,
+      "thisisthesecuritytokengeneratedbyme"
+    );
 
     console.log("Decoded token:", decodedToken);
 
     if (!decodedToken) {
       return res.status(401).json({ message: "Invalid token" });
     }
-    res.cookie('jwt', token, {
+    res.cookie("jwt", token, {
       httpOnly: true,
-
     });
 
-    return res.status(200).json({ userId: user._id, message: "Login successful" });
-
+    return res
+      .status(200)
+      .json({ userId: user._id, message: "Login successful" });
   } catch (err) {
     console.error("Error:", err);
     return res.status(500).json({ message: "Internal server error" });
@@ -181,37 +197,49 @@ app.post("/email_check", async (req, res) => {
   }
 });
 
-app.post('/store-profile', upload.single('profilePicture'), async (req, res) => {
-  try {
-      const token = req.cookies.token; 
+app.post(
+  "/store-profile",
+  upload.single("profilePicture"),
+  async (req, res) => {
+    try {
+      const token = req.cookies.token;
       const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
-      
+
       if (!decodedToken) {
-          return res.status(401).json({ success: false, message: "Invalid token" });
+        return res
+          .status(401)
+          .json({ success: false, message: "Invalid token" });
       }
 
       const userId = decodedToken._id;
       const profilePicture = req.file ? req.file.path : null;
-      const productImages = req.body.productImages ? JSON.parse(req.body.productImages) : [];
+      const productImages = req.body.productImages
+        ? JSON.parse(req.body.productImages)
+        : [];
       const skills = req.body.skills;
       const expertise = req.body.expertise;
 
       const profileData = {
-          userId: userId,
-          profilePicture: profilePicture,
-          productImages: productImages,
-          skills: skills,
-          expertise: expertise
+        userId: userId,
+        profilePicture: profilePicture,
+        productImages: productImages,
+        skills: skills,
+        expertise: expertise,
       };
 
       await Profile.create(profileData);
 
-      res.status(200).json({ success: true, message: 'Profile data stored successfully' });
-  } catch (err) {
-      console.error('Error saving profile:', err);
-      res.status(500).json({ success: false, message: 'Internal Server Error' });
+      res
+        .status(200)
+        .json({ success: true, message: "Profile data stored successfully" });
+    } catch (err) {
+      console.error("Error saving profile:", err);
+      res
+        .status(500)
+        .json({ success: false, message: "Internal Server Error" });
+    }
   }
-});
+);
 
 app.post("/modify", async (req, res) => {
   try {
